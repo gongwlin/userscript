a4e036d069f9a755220298785bd5ca1e3891bc37
gongwlin <gongwlin@126.com>
2022-09-14 19:56:48 +0800

    feat: zh.js

---
+++ zh.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         知乎网站 辅助工具
+// @namespace    https://git-opd.nie.netease.com/fe/infra/tampermonkey/gitlab-pipeline
+// @version      1.1.1
+// @description  知乎网站 辅助工具
+// @author       gwl
+// @match        *://.*zhihu.com/*
+// @updateURL    https://git-opd.nie.netease.com/fe/infra/tampermonkey/gitlab-pipeline/-/raw/master/gitlab-pipeline.user.js
+// @downloadURL  https://git-opd.nie.netease.com/fe/infra/tampermonkey/gitlab-pipeline/-/raw/master/gitlab-pipeline.user.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  console.log("zhihu script running...");
+})();
