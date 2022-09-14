2e1313286aeba305c20f3bb7313dc1ef522caad7
gongwlin <gongwlin@126.com>
2022-09-14 20:01:13 +0800

    feat: update zh.js

--- zh.js
+++ zh.js
@@ -1,15 +1,28 @@
 // ==UserScript==
 // @name         知乎网站 辅助工具
-// @namespace    https://git-opd.nie.netease.com/fe/infra/tampermonkey/gitlab-pipeline
+// @namespace    https://github.com/gongwlin/userscript
 // @version      1.1.1
 // @description  知乎网站 辅助工具
 // @author       gwl
 // @match        *://.*zhihu.com/*
-// @updateURL    https://git-opd.nie.netease.com/fe/infra/tampermonkey/gitlab-pipeline/-/raw/master/gitlab-pipeline.user.js
-// @downloadURL  https://git-opd.nie.netease.com/fe/infra/tampermonkey/gitlab-pipeline/-/raw/master/gitlab-pipeline.user.js
+// @updateURL    https://github.com/gongwlin/userscript/blob/main/zh.js
+// @downloadURL  https://github.com/gongwlin/userscript/blob/main/zh.js
 // @grant        none
 // ==/UserScript==
 
 (function () {
   console.log("zhihu script running...");
+
+  window.addEventListener('click', function (e) {
+    const href = e.target?.href || '';
+    if (href?.indexOf('https://link.zhihu.com/?target=') !== -1) {
+      e.preventDefault();
+      e.stopPropagation();
+      const url = href.split('https://link.zhihu.com/?target=')[1];
+      console.log('url', url)
+      window.open(decodeURIComponent(url));
+    }
+ 
+  });
+
 })();
