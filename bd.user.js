5b8a98b4b71439acdd0ff1c3ec06bc2c38b7ef22
gongwlin <gongwlin@126.com>
2022-10-01 15:22:40 +0800

    feat: add

---
+++ bd.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         去除百度无用内容
+// @namespace    https://github.com/gongwlin/userscript
+// @version      1.0.0
+// @author       gwl
+// @include        *://*baidu.com/s*
+// @updateURL    https://github.com/gongwlin/userscript/raw/main/bd.user.js
+// @downloadURL  https://github.com/gongwlin/userscript/raw/main/bd.user.js
+// @grant        none
+// ==/UserScript==
+
+const node = document.querySelector('#content_right')
+if (node) {
+  node.parentNode.removeChild(node)
+}
