81b03d24069b7d222603d37d2b94825861f56698
gongwlin <gongwlin@126.com>
2025-04-04 09:27:13 +0800

    feat

--- anna.user.js
+++ anna.user.js
@@ -1,23 +1,24 @@
 // ==UserScript==
 // @name         安娜自动下载
 // @namespace    https://github.com/gongwlin/userscript
-// @version      1.0.0
+// @version      1.1.0
 // @updateURL    https://github.com/gongwlin/userscript/raw/main/anna.user.js
 // @downloadURL  https://github.com/gongwlin/userscript/raw/main/anna.user.js
 // @description  安娜自动下载
 // @author       gwl
-// @match        *://*annas-archive.org/slow_download*/*
+// @include      *://*annas-archive.org/slow_download/*
 // @grant        GM_log
 // @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        unsafeWindow
-// @run-at       document-end
+// @run-at       document-start
 // @license      MIT
 // ==/UserScript==
 
 
 const id = setInterval(() => {
+    console.log('start countdown')
     const dom = document.querySelector('.mb-4.text-xl.font-bold a');
     if (dom) {
         dom.click();
