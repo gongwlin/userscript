ed763210ed7fc2b7cc4582fe62f92a0aa107b562
gongwlin <gongwlin@126.com>
2025-09-07 09:14:57 +0800

    feat

--- anna.user.js
+++ anna.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         安娜自动下载
 // @namespace    https://github.com/gongwlin/userscript
-// @version      1.2.0
+// @version      1.3.0
 // @updateURL    https://github.com/gongwlin/userscript/raw/main/anna.user.js
 // @downloadURL  https://github.com/gongwlin/userscript/raw/main/anna.user.js
 // @description  安娜自动下载
 // @author       gwl
 // @include      *://*annas-archive.org/slow_download/*
+// @include      *://*zh.annas-archive.org/md5/*
 // @grant        GM_log
 // @grant        GM_addStyle
 // @grant        GM_setValue
@@ -25,3 +26,27 @@ const id = setInterval(() => {
     }
 }, 1e3)
 
+
+const id2 = setInterval(() => {
+    const list =  document.querySelectorAll('.list-inside')?.[1];
+    if (!list) {
+        return
+    }
+    const dom = list.querySelector('.list-disc a')
+    console.log(dom)
+    if (dom) {
+        dom.click();
+        clearInterval(id2);
+    }
+}, 1e3)
+
+const yzm = setInterval(() => {
+    const dom = document.querySelector('#LsMgo8');
+    if (!dom) {
+        return
+    }
+    navigator.clipboard.writeText(`captcha`).catch(() => {
+        alert('复制失败');
+    });
+
+}, 1e3)
