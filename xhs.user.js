2b36bdc8f9f9d3b864aff89f96a66fb4d34829e3
gongwlin <gongwlin@126.com>
2025-09-21 07:25:10 +0800

    feat

---
+++ xhs.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         安娜自动下载
+// @namespace    https://github.com/gongwlin/userscript
+// @version      0.0.1
+// @updateURL    https://github.com/gongwlin/userscript/raw/main/xhs.user.js
+// @downloadURL  https://github.com/gongwlin/userscript/raw/main/xhs.user.js
+// @description  小红书防沉迷，只展示前10个帖子
+// @author       gwl
+// @include      *://xiaohongshu.com/explore/*
+
+// @grant        GM_log
+// @grant        GM_addStyle
+// @grant        unsafeWindow
+
+// @run-at       document-start
+// @license      MIT
+// ==/UserScript==
+
+
+setInterval(() => {
+  const url = window.location.href;
+  if (url.indexOf('xsec_token') > 0) {
+    return;
+  }
+  const list = document.querySelectorAll('.note-item');
+  Array.from(list).forEach((v, i) => {if (i > 8) v.remove()})
+}, 1000)
