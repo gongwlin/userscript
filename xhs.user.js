b067b2b57a02de219aca3224ad5cbd09c4d8273c
gongwlin <gongwlin@126.com>
2025-09-21 09:28:16 +0800

    feat

--- xhs.user.js
+++ xhs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         小红书防沉迷，只展示前10个帖子
 // @namespace    https://github.com/gongwlin/userscript
-// @version      0.0.6
+// @version      0.0.7
 // @updateURL    https://github.com/gongwlin/userscript/raw/main/xhs.user.js
 // @downloadURL  https://github.com/gongwlin/userscript/raw/main/xhs.user.js
 // @description  小红书防沉迷，只展示前10个帖子
