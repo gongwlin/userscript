b551c1a4c3856186f125f85a277c7fca48ccdf03
gongwlin <gongwlin@126.com>
2025-09-21 09:34:15 +0800

    feat

--- xhs.user.js
+++ xhs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         小红书防沉迷，只展示前10个帖子
 // @namespace    https://github.com/gongwlin/userscript
-// @version      0.0.7
+// @version      0.0.8
 // @updateURL    https://github.com/gongwlin/userscript/raw/main/xhs.user.js
 // @downloadURL  https://github.com/gongwlin/userscript/raw/main/xhs.user.js
 // @description  小红书防沉迷，只展示前10个帖子
@@ -18,12 +18,9 @@
 
 const id = setInterval(() => {
   const url = window.location.href;
-  console.log('==============')
   if (url.indexOf('xsec_token') > 0) {
-    clearInterval(id)
     return;
   }
-  console.log('---------')
   const list = document.querySelector('#exploreFeeds').children;
   Array.from(list).forEach((v, i) => {if (i > 6) v.remove()});
 }, 500)
