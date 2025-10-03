58e16eecc599c6f772304e820ce65218d7f5f20d
gongwlin <gongwlin@126.com>
2025-10-03 14:01:41 +0800

    feat

--- xhs.user.js
+++ xhs.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         小红书防沉迷，只展示前10个帖子
 // @namespace    https://github.com/gongwlin/userscript
-// @version      1.0.2
+// @version      1.0.3
 // @updateURL    https://github.com/gongwlin/userscript/raw/main/xhs.user.js
 // @downloadURL  https://github.com/gongwlin/userscript/raw/main/xhs.user.js
 // @description  小红书防沉迷，只展示前10个帖子
 // @author       gwl
 // @include      https://*xiaohongshu.com/explore*
+// @include      https://*bilibili.com*
 
 // @grant        GM_log
 // @grant        unsafeWindow
@@ -15,24 +16,46 @@
 // @license      MIT
 // ==/UserScript==
 
+const url = window.location.href;
+if (/xiaohongshu\.com/.test(url)) {
+	const xhs_timerId = setInterval(() => {
+		if (url.indexOf("xsec_token") > 0) {
+			return;
+		}
+		const feedsDom = document.querySelector("#exploreFeeds");
+		if (!feedsDom) {
+			return;
+		}
+		const ll = feedsDom.children;
+		Array.from(ll).forEach((v, i) => {
+			if (i > 6) {
+				v.style.opacity = 0;
+			}
+		});
+	}, 500);
 
-const xhs_timerId = setInterval(() => {
-  const url = window.location.href;
-  if (url.indexOf('xsec_token') > 0) {
-    return;
-  }
-  const ll = document.querySelector('#exploreFeeds').children;
-  Array.from(ll).forEach((v, i) => {if (i > 10) v.remove()});
-}, 500)
-
-
-const attribute = 'position: fixed;display: flex;bottom: 20px;right: 20px;width: 30px;height: 30px;cursor: pointer;border-radius: 50%;background-color: brown;'
-const prop = attribute.split(';').filter(Boolean);
-const occc = {};
-const aaa = document.createElement('div');
-aaa.onclick = function () {
-  clearInterval(xhs_timerId)
+	const attribute =
+		"position: fixed;display: flex;bottom: 20px;right: 20px;width: 30px;height: 30px;cursor: pointer;border-radius: 50%;background-color: brown;";
+	const prop = attribute.split(";").filter(Boolean);
+	const occc = {};
+	const aaa = document.createElement("div");
+	aaa.onclick = function () {
+		clearInterval(xhs_timerId);
+	};
+	prop.forEach((v) => {
+		const [key, val] = v.split(":");
+		occc[key] = val.replaceAll(" ", "");
+	});
+	Object.keys(occc).forEach((k) => {
+		aaa.style[k] = occc[k];
+	});
+	document.body.appendChild(aaa);
 }
-prop.forEach(v => { const [key, val] = v.split(':'); occc[key] = val.replaceAll(' ', '')  });
-Object.keys(occc).forEach((k) => { aaa.style[k] = occc[k]  });
-document.body.appendChild(aaa);
+
+if (/bilibili\.com/.test(url)) {
+	setInterval(() => {
+		document
+			.querySelectorAll(".feed-card")
+			.forEach((v) => (v.style.opacity = 0));
+	}, 200);
+}
