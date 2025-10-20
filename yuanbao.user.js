c5d7b9fcd8860781fbf3902d8706525629ccdbe9
gongwlin <gongwlin@126.com>
2025-10-20 15:56:21 +0800

    feat

--- yuanbao.user.js
+++ yuanbao.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         yuanbao remove
 // @namespace    https://github.com/gongwlin/userscript
-// @version      0.0.1
+// @version      0.0.2
 // @updateURL    https://github.com/gongwlin/userscript/raw/main/yuanbao.user.js
 // @downloadURL  https://github.com/gongwlin/userscript/raw/main/yuanbao.user.js
 // @description  yuanbao remove
@@ -20,9 +20,7 @@
 // ==/UserScript==
 
 
-const yzm = setInterval(() => {
-    GM_setClipboard('@@removeyb');
-    setTimeout(() => {
-        clearInterval(yzm)
-    }, 5e3)
+GM_setClipboard('@@removeyb');
+const yzm = setTimeout(() => {
+   GM_setClipboard('@@removeyb');
 }, 2e3)
