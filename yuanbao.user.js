3616a8f98e7a2e3b8233ff12fccee923d4440c3c
gongwlin <gongwlin@126.com>
2025-10-26 21:41:39 +0800

    feat

--- yuanbao.user.js
+++ yuanbao.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         yuanbao remove
 // @namespace    https://github.com/gongwlin/userscript
-// @version      0.0.6
+// @version      0.0.8
 // @updateURL    https://github.com/gongwlin/userscript/raw/main/yuanbao.user.js
 // @downloadURL  https://github.com/gongwlin/userscript/raw/main/yuanbao.user.js
 // @description  yuanbao remove
@@ -20,19 +20,20 @@
 // ==/UserScript==
 
 function m() {
-   const v = GM_getValue('ybdate');
-   const today = (new Date()).getDate();
-   const s = String(today);
-   if (v === s) {
-      return
-   }
-   GM_setValue('ybdate', s)
-   setTimeout(() => {
-      GM_setClipboard('@@removeyb');
-   }, 1e3)
-   
+	const v = GM_getValue("ybdate");
+	const today = new Date().getDate();
+	const s = String(today);
+	if (v === s) {
+		return;
+	}
+	GM_setValue("ybdate", s);
+	setTimeout(() => {
+		GM_setClipboard("@@removeyb");
+	}, 1e3);
+
+	setTimeout(() => {
+		GM_setClipboard("@@removeyb");
+	}, 3e3);
 }
 
-m()
-
-
+m();
