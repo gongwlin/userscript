f5f16038bc882f76624534baf98ffb96f726ce10
gongwlin <gongwlin@126.com>
2026-02-23 17:06:06 +0800

    feat: update

---
+++ thegu.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         theguardian remove
+// @namespace    https://github.com/gongwlin/userscript
+// @version      0.0.1
+// @updateURL    https://github.com/gongwlin/userscript/raw/main/thegu.user.js
+// @downloadURL  https://github.com/gongwlin/userscript/raw/main/thegu.user.js
+// @description  theguardian remove
+// @author       gwl
+// @include      *://*theguardian*
+// @grant        GM_log
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        unsafeWindow
+// @run-at       document-start
+// @license      MIT
+// ==/UserScript==
+
+
+document.querySelectorAll('aside').forEach(v => v.remove())
